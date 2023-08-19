67a41671c39fb82e451331a467c14d42bc2745eb
mateussoaresdev <mateussoaresst@gmail.com>
2023-08-19 13:28:26 -0300

    Resolvendo desafio ListaTarefa
    Signed-off-by: mateussoaresdev <mateussoaresst@gmail.com>

---
+++ src/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,61 @@
+package OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListaTarefa {
+    //atributes
+
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t: tarefaList) {
+            if(t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalTarefas() {
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas() {
+        System.out.println(tarefaList);
+    }
+
+    public List<Tarefa> getTarefaList() {
+        return tarefaList;
+    }
+
+    public void setTarefaList(List<Tarefa> tarefaList) {
+        this.tarefaList = tarefaList;
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        listaTarefa.adicionarTarefa("Tarefa 3");
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 2");
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());;
+
+        listaTarefa.obterDescricoesTarefas();
+    }
+
+}
